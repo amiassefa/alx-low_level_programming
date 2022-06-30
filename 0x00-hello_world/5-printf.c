5c9f27aefbb504bde368ab0b0df2ecb0ffcd9275
amiassefa <amiassefa@gmail.com>
2022-06-30 06:10:25 -0700

    test printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdio.h>                                                                                            
+/**                                                                                                           
+ *  *main - Entry point                                                                                          
+ *   *Return: Always 0 (Success)                                                                                  
+ **/ 
 int main(void)
 {
-     char tmp[] = "with proper grammar, but the outcome is a piece of art,";
-
-     printf("%s", tmp);
-
-     printf("\n");
-
-     return (0);
+	char tmp[] = "with proper grammar, but the outcome is a piece of art,";
+	printf("%s", tmp);
+	printf("\n");
+	return (0);
 }
