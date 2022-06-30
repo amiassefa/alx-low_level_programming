e0babc5fa4fb1f81e35e936f115ad3a88f41b705
amiassefa <amiassefa@gmail.com>
2022-06-30 02:58:16 -0700

    test printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,9 @@ int main(void)
 {
      char tmp[] = "with proper grammar, but the outcome is a piece of art,";
 
-     printf("%s",tmp);
+     printf("%s", tmp);
 
      printf("\n");
 
-     return 0;	
+     return (0);
 }
