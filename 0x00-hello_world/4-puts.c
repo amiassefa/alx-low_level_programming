042f6de0e11bcf4ddbfd743e7589c259ab0edafb
amiassefa <amiassefa@gmail.com>
2022-06-30 02:29:24 -0700

    First C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+    {
+         char Mystr[] = "Programming is like building a multilingual puzzle";
+         /*int val = puts(Mystr);*/
+	 puts(Mystr);
+	 return 0;
+    }
