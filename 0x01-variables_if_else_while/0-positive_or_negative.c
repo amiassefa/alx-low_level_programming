a94406500698cb45504a560d3d7d344acc21c690
amiassefa <amiassefa@gmail.com>
2022-07-01 05:56:15 -0700

    branching ..

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,12 +10,11 @@ int main(void)
 	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if(n>0)
-		printf("%d%s",n, " is positive\n");
+		printf("%d%s", n, " is positive\n");
 	else if (n==0)
-		printf("%d%s",n, " is zero\n");
+		printf("%d%s", n, " is zero\n");
 	else
-		printf("%d%s",n, " is negative\n");
+		printf("%d%s", n, " is negative\n");
 	return (0);
 }
