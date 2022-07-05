297840cc27c42975ac132207d5cfa1e7758ccf73
amiassefa <amiassefa@gmail.com>
2022-07-05 06:08:21 -0700

    Fibonaccis 3

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,14 +11,14 @@ int main(void)
 	while (i < 98)
 	{
 		if (i == 0)
-			printf("%ld", j);
+			printf("%lu", j);
 		else if (i == 1)
-			printf(", %ld", k);
+			printf(", %lu", k);
 		else
 		{
 			k += j;
 			j = k - j;
-			printf(", %ld", k);
+			printf(", %lu", k);
 		}
 		++i;
 	}
