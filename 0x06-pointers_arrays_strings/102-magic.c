f708106132b63c0cbc9c32614b3f2bf24ba648f8
amiassefa <amiassefa@gmail.com>
2022-08-01 08:28:46 +0300

    102-magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Alx students.
- *
- * Return: Always 0.
+ * main -> main function
+ * Return: 0 success
  */
 int main(void)
 {
@@ -13,16 +12,11 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-	/*
-	 *write your line of code here...
-	 *Remember:
-	 *- you are not allowed to use a
-	 *- you are not allowed to modify p
-	 *- only one statement
-	 *- you are not allowed to code anything else than this line of code
+	/**
+	 * write your line of code here...
+	 * Remember:
 	 */
-	*(p + 5) = 98;
-	;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
